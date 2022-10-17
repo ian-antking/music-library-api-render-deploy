0f7c412cfe6f949ec46aaa281ed0b6a40d80d8ca
Ian King <ian.antking@gmail.com>
2022-10-17 17:27:34 +0100

    Revert "dont run song tests"
    This reverts commit f6f825517e6bed60aad15ce313ae7f8bf19f6699.

--- tests/songs.test.js
+++ tests/songs.test.js
@@ -1,56 +1,56 @@
-// const { expect } = require('chai');
-// const request = require('supertest');
-// const app = require('../src/app');
-// const { Artist, Album, Song } = require('../src/models');
+const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../src/app');
+const { Artist, Album, Song } = require('../src/models');
 
-// describe('/songs', () => {
-//   let artist;
-//   let album;
+describe('/songs', () => {
+  let artist;
+  let album;
 
-//   before((done) => {
-//     Promise.all([
-//       Song.sequelize.sync(),
-//       Album.sequelize.sync(),
-//       Artist.sequelize.sync()
-//     ])
-//       .then(() => done())
-//       .catch((error) => done(error));
-//   });
+  before((done) => {
+    Promise.all([
+      Song.sequelize.sync(),
+      Album.sequelize.sync(),
+      Artist.sequelize.sync()
+    ])
+      .then(() => done())
+      .catch((error) => done(error));
+  });
 
-//   beforeEach((done) => {
-//     Promise.all([
-//       Artist.truncate({ cascade: true }),
-//       Album.truncate({ cascade: true }),
-//       Song.truncate({ cascade: true })
-//     ])
-//       .then(() => Artist.create({ name: "Tame Impala", genre: "Rock" }))
-//       .then((artistDocument) => {
-//         artist = artistDocument;
-//       })
-//       .then(() => Album.create({ name: 'InnerSpeaker', year: 2010, artistId: artist.id }))
-//       .then((albumDocument) => {
-//         album = albumDocument;
-//         done();
-//       })
-//       .catch((error) => done(error));
-//   });
+  beforeEach((done) => {
+    Promise.all([
+      Artist.truncate({ cascade: true }),
+      Album.truncate({ cascade: true }),
+      Song.truncate({ cascade: true })
+    ])
+      .then(() => Artist.create({ name: "Tame Impala", genre: "Rock" }))
+      .then((artistDocument) => {
+        artist = artistDocument;
+      })
+      .then(() => Album.create({ name: 'InnerSpeaker', year: 2010, artistId: artist.id }))
+      .then((albumDocument) => {
+        album = albumDocument;
+        done();
+      })
+      .catch((error) => done(error));
+  });
 
-//   describe('POST /album/:albumId/song', () => {
-//     it('creates a new song under an album', (done) => {
-//       request(app)
-//         .post(`/albums/${album.id}/songs`)
-//         .send({
-//           artistId: artist.id,
-//           name: 'Solitude Is Bliss',
-//         })
-//         .then((res) => {
-//           expect(res.status).to.equal(201);
-//           expect(res.body.name).to.equal('Solitude Is Bliss');
-//           expect(res.body.albumId).to.equal(album.id);
-//           expect(res.body.artistId).to.equal(artist.id);
-//           done();
-//         })
-//         .catch((error) => done(error));
-//     });
-//   });
-// });
+  describe('POST /album/:albumId/song', () => {
+    it('creates a new song under an album', (done) => {
+      request(app)
+        .post(`/albums/${album.id}/songs`)
+        .send({
+          artistId: artist.id,
+          name: 'Solitude Is Bliss',
+        })
+        .then((res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body.name).to.equal('Solitude Is Bliss');
+          expect(res.body.albumId).to.equal(album.id);
+          expect(res.body.artistId).to.equal(artist.id);
+          done();
+        })
+        .catch((error) => done(error));
+    });
+  });
+});

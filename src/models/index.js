f4d8e8f537bc61fb685a96d17a824bd873e2fc88
Ian King <ian.antking@gmail.com>
2022-10-17 15:54:19 +0100

    remove node_env

--- src/models/index.js
+++ src/models/index.js
@@ -3,7 +3,7 @@ const ArtistModel = require('./artist');
 const AlbumModel = require('./album');
 const SongModel = require('./song');
 
-const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, NODE_ENV } = process.env;
+const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const setupDatabase = () => {
   const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {

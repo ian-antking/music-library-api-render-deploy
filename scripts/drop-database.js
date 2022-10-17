659b7145bc365eba5d5c3c8ceeb718f56e825113
Ian King <ian.antking@gmail.com>
2022-10-17 15:50:34 +0100

    remove mysql2

--- scripts/drop-database.js
+++ scripts/drop-database.js
@@ -1,17 +1,18 @@
-const mysql = require('mysql2');
-const path = require('path');
+console.log("TODO: swap scripts to postgres")
+// const mysql = require('mysql2');
+// const path = require('path');
 
-require('dotenv').config({
-  path: path.join(__dirname, '../.env.test'),
-});
+// require('dotenv').config({
+//   path: path.join(__dirname, '../.env.test'),
+// });
 
-const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
+// const { DB_PASSWORD, DB_NAME, DB_USER, DB_HOST, DB_PORT } = process.env;
 
-const connection = mysql.createConnection({
-  host: DB_HOST,
-  user: DB_USER,
-  password: DB_PASSWORD,
-  port: DB_PORT,
-});
+// const connection = mysql.createConnection({
+//   host: DB_HOST,
+//   user: DB_USER,
+//   password: DB_PASSWORD,
+//   port: DB_PORT,
+// });
 
-connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
+// connection.query(`DROP DATABASE ${DB_NAME}`, () => connection.end());
